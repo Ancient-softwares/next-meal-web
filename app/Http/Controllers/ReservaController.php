aa9ae640bfe57ddd6ff1fa320a66b8214e924588
Athirson <athirsonarceus@gmail.com>
2022-09-11 20:04:40 -0300

    reservaController

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ReservaModel;
 
 class ReservaController extends Controller
 {
-    //
+    private $reservas;
+
+    public function reserva(Request $request) {
+        $dataReserva = $request->dataReserva;
+        $horaReserva = $request->horaReserva;
+        $numPessoas = $request->numPessoas;
+
+        $reserva = $this->reservas->create([
+            "dataReserva" => $dataReserva,
+            "horaReserva" => $horaReserva,
+            "numPessoas" => $numPessoas
+        ]);
+    }
 }
