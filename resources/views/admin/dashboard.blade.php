b4c53c4d304ebe07c1d1710df2fa8265b10b239d
CaiqueJar <caiquedm11@gmail.com>
2022-11-10 21:24:53 -0300

    input type email estilizado

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,6 +7,27 @@
 @section('conteudo')
 <div class="container-cont">
 <h1>Área do administrador</h1>
-<p>Vai vir gráfico aqui</p>
+<br>
+<div class="row">
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Quantidade de clientes</h5>
+              <p class="card-text">5 bilhões</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Quantidade de restaurante</h5>
+              <p class="card-text">5 bilhões</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
 </div>
 @endsection
