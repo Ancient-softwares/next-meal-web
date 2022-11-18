8073695c70cf215b0a3f2d0e6e97631892141b68
brunopires002 <brunoteixeirapires002@gmail.com>
2022-11-18 17:34:39 -0300

    Responsividade tela reserva.

--- resources/views/reserva.blade.php
+++ resources/views/reserva.blade.php
@@ -24,52 +24,56 @@
     <div class="reserva">
         <h1>Reservas</h1>
     </div>
-    <div class="global">
-        <h2>Pendentes</h2>
-        <div class="aceitar-reserva scroll">
-            @foreach($reservas as $r)
-            @if($r->idStatusReserva == 3)
-            <div class="card-group">
-                <div class="card">
-                    <img src="{{ asset('img/perfil.png') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $clientes->where('idCliente', $r->idCliente)->first()->nomeCliente }}</h5>
-                        <p class="card-text">Data: {{$r->dataReserva}} {{$r->horaReserva}}</p>
-                        <p class="card-text">Mesa: {{$r->numPessoas}} acentos.</p>
-                        <p class="card-text">Status: {{ $status->where('idStatusReserva', $r->idStatusReserva)->first()->statusReserva }}.</p>
 
-                    </div>
-                    <div class="botoes">
-                        <a href="{{ route('aceitar-reserva', ['id'=>$r->idReserva]) }}" class="btn btn-light">
-                            Aceitar
-                        </a>
-                        <a href="{{ route('rejeitar-reserva', ['id'=>$r->idReserva]) }}" class="btn btn-light">
-                            Recusar
-                        </a>
-                    </div>
-                </div>
-            </div>
-            @endif
-            @endForeach
-        </div>
-
-        
+    <div class="title">
         <h2>Aceitas</h2>
-        <div class="reservados scroll-lateral">
-            <div class="row row-cols-1 row-cols-md-6 g-4">
+        <h2>Pendentes</h2>
+    </div>
+    <div class="global">
+        <div class="aceitar-reserva scroll">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
                 @foreach($reservas as $r)
-                @if($r->idStatusReserva == 1)
-                
-                    <div class="col">
-                        <div class="card">
-                            <img src="{{ asset('img/perfil.png') }}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $clientes->where('idCliente', $r->idCliente)->first()->nomeCliente }}</h5>
-                                <p class="card-text">Data: {{$r->dataReserva}} {{$r->horaReserva}}</p>
-                                <p class="card-text">Mesa: {{$r->numPessoas}} acentos</p>
-                            </div>
+                @if($r->idStatusReserva == 3)
+                <div class="card-group">
+                    <div class="card">
+                        <img src="{{ asset('img/perfil.png') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $clientes->where('idCliente', $r->idCliente)->first()->nomeCliente }}</h5>
+                            <p class="card-text">Data: {{$r->dataReserva}} {{$r->horaReserva}}</p>
+                            <p class="card-text">Mesa: {{$r->numPessoas}} acentos.</p>
+                            <p class="card-text">Status: {{ $status->where('idStatusReserva', $r->idStatusReserva)->first()->statusReserva }}.</p>
+
+                        </div>
+                        <div class="botoes">
+                            <a href="{{ route('aceitar-reserva', ['id'=>$r->idReserva]) }}" class="btn btn-light">
+                                Aceitar
+                            </a>
+                            <a href="{{ route('rejeitar-reserva', ['id'=>$r->idReserva]) }}" class="btn btn-light">
+                                Recusar
+                            </a>
                         </div>
                     </div>
+                </div>
+                @endif
+                @endForeach
+            </div>
+        </div>
+
+        <div class="reservados scroll">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @foreach($reservas as $r)
+                @if($r->idStatusReserva == 1)
+
+                <div class="col">
+                    <div class="card">
+                        <img src="{{ asset('img/perfil.png') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $clientes->where('idCliente', $r->idCliente)->first()->nomeCliente }}</h5>
+                            <p class="card-text">Data: {{$r->dataReserva}} {{$r->horaReserva}}</p>
+                            <p class="card-text">Mesa: {{$r->numPessoas}} acentos</p>
+                        </div>
+                    </div>
+                </div>
                 @endif
                 @endForeach
             </div>
